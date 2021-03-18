43baef1565abeec0798ee73389c3e52f4eb9363d
liumingxing <1421053434@qq.com>
2021-03-18 09:49:25 +0800

    添加一些绘图注释

--- application/handwrittenNumeralRecognition.m
+++ application/handwrittenNumeralRecognition.m
@@ -1,6 +1,7 @@
 function handwrittenNumeralRecognition()
     close all;
     clear all;
+    addpath('../');
 %
     % -1 代表输入层，0代表卷积层，1代表池化层，2代表全连接层，3代表softmax输出层
     layerTypes   = [-1,            0,           1              0             1         2            3]
@@ -16,14 +17,22 @@ function handwrittenNumeralRecognition()
 %   载入训练集和测试集
     load('../fixDataSets/handwrittenDigit/data')
     m=length(Y);
-    disp(sprintf('训练集样本大小%d',m))
     testmM=length(testY);
-    disp(sprintf('测试集样本大小%d',testmM))
 
     % 初始化参数
     wb = initializeParameters(L,layerTypes,layerNeruals);
 
+    m = 10;
+    x=x(:,:,1,1:m);
+    y=y(:,1:m);
+    Y=Y(1:m);
+    testmM=10;
+    testx=testx(:,:,1,1:testmM);
+    testy=testy(:,1:testmM);
+    testY=testY(1:testmM);
 
+    disp(sprintf('训练集样本大小%d',m))
+    disp(sprintf('测试集样本大小%d',testmM))
 
     start = time();
 %    记录了每次迭代的cost
@@ -41,7 +50,7 @@ function handwrittenNumeralRecognition()
 %     训练批次
     epoths = 2;
     % 迷你训练批次大小,总训练集大小为60000 = 100*600
-    miniBatchSize = 100;
+    miniBatchSize = 2;
     betterWB = wb;
     betterWBs = [];
     accuracies = [];
@@ -104,9 +113,9 @@ function handwrittenNumeralRecognition()
     fprintf('训练结束，总耗时: %4.6e\n',trainCostTime);
 
 
-    fileReport = sprintf('report/fileReport %s.txt',datestr(now))
+    fileReport = sprintf('../report/fileReport %s.txt',datestr(now))
     save(fileReport,"L","layerTypes","layerNeruals","ps","betterWBs","accuracies","testAccuracies","perEpothCostTms","costs","USCosts","testCosts");
-
+% 绘制 cost 随 iterations 的变化
     figure(1);
     hold on;
     plot([0:(length(costs)-1)],costs);
@@ -114,7 +123,7 @@ function handwrittenNumeralRecognition()
     xlabel('iterations');
     ylabel('costs');
     hold off;
-
+% 绘制 train 和 test 集 上 cost 随 epoths 的变化
     figure(2);
     hold on;
     plot([0:(length(USCosts)-1)],USCosts);
@@ -125,7 +134,7 @@ function handwrittenNumeralRecognition()
     ylabel('costs');
     hold off;
 
-
+% 绘制 train 和 test 集 上 accuracy 随 epoths 的变化
     figure(3);
     hold on;
     plot([0:(length(accuracies)-1)],accuracies);
@@ -135,7 +144,7 @@ function handwrittenNumeralRecognition()
     xlabel('epoths');
     ylabel('accuracy');
     hold off;
-
+% 绘制 train 和 test 集 上 耗时 随 epoths 的变化
     figure(4);
     hold on;
     plot([0:(length(perEpothCostTms)-1)],perEpothCostTms);
